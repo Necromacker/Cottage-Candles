d01443d57fab44bafa77be203157c795483c4ead
Krishna Gorde <krishnagorde04@gmail.com>
2025-09-08 01:29:10 +0530

    About & Collections Added
    About and collections section is now complete :)

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,10 +32,6 @@ let testimonialSwiper = new Swiper(".testimonial-swiper", {
   spaceBetween: 30,
   loop: "true",
 
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
 });
 
 /*=============== NEW SWIPER ===============*/
